84cdbc2cb17624bae0716d0b6694dcbc78a99f6f
henryemily098 <71964325+henryemily098@users.noreply.github.com>
2021-04-28 11:27:23 +0700

    🛠 Fixing some bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login('ODM2ODE0MTgyMDYxMzA5OTcz.YIjd8A.IWK561R3WDA7-1IVFuOsbbkLMm4');
+client.login('your-token');
 
 client.on('ready', () =>{ 
     console.log(`${client.user.tag} its ready`);
@@ -12,15 +12,19 @@ client.on('ready', () =>{
 // This is where the stats works
 
 let stats = {
-    Guild_ID: "772740285221699624",
-    Total_Members: "836812056710742026",
-    Members: "836812091082932304",
-    Bots: "836812136054259785",
+    Guild_ID: "", // Guild ID
+    Total_Members: "", // For Total Members Channel ID
+    Members: "", // For Members Channel ID
+    Bots: "", // For Bots Channel ID
 };
 
 async function ServerStats() {
+    // Getting Guild
     var Guild = client.guilds.cache.get(stats.Guild_ID);
+    // Getting Total members channel for MemberCount
     await Guild.channels.cache.get(stats.Total_Members).setName('Total Members: ' + Guild.memberCount);
+    // Getting Members channel for users count
     await Guild.channels.cache.get(stats.Members).setName('Members: ' +  Guild.members.cache.filter(m => !m.user.bot).size);
+    // Getting Bots channel for bots count
     await Guild.channels.cache.get(stats.Bots).setName('Bots: ' + Guild.members.cache.filter(m => m.user.bot).size);
 }
